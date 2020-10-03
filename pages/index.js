9aa35806fe4f59cea9ebfff910e9a7fa598cdcfb
Camila <camilaebf@gmail.com>
2020-10-03 20:04:27 -0300

    push to prod all changes

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,37 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>2020 made me do this.</p>
+          <p>
+            I will be posting my notes from{' '}
+            <a href="https://collectednotes.com/camilaebf">Collected Notes</a> here.
+          </p>
+          <p>
+            You can find out more about me on{' '}
+            <a href="https://www.linkedin.com/in/camilablancfick/">LinkedIn</a> or{' '}
+            <a href="https://twitter.com/Cami__bf">follow me on Twitter</a>.
+          </p>
+        </section>
+      </Layout>
 
       <footer className={styles.footer}>
+        <a
+          href="https://github.com/CamilaEBF"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Camilaebf --
+        </a>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -59,6 +40,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
+        
       </footer>
     </div>
   )
